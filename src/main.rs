0fe11f43dde93969b59f2adb5513e7a49016677c
Ericky <ericky_k_y@hotmail.com>
2025-06-02 10:30:10 -0400

    feat: add version output logging when first starting

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ use crate::plugin::dbus::{build_connection, register_plugin};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let version = env!("CARGO_PKG_VERSION");
+
     env::set_var("RUST_LOG", "debug");
     // Configure logging to output to stdout
     env_logger::builder()
         .target(env_logger::Target::Stdout)
         .init();
 
-    log::info!("Starting Playtron Plugin");
+    log::info!("Starting Playtron Plugin version: {version}");
 
     let local_service = LocalService::new();
     build_connection(local_service).await?;
